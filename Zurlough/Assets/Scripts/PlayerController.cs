4c9be5565b9d7b7649a20e3867f26f76548ced5f
Your Name <you@example.com>
2021-05-28 02:19:06 +0100

    Prototype scene pre animation

---
+++ Zurlough/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed = 50.0f;
+    public float verticalInput;
+    public float horizontalInput;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+
+    }
+}
---
+++ Zurlough/Assets/Shared/PlayerMovement3D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement3D : MonoBehaviour
+{
+    public float speed = 20;
+    private Vector3 motion;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();  
+    }
+
+    void Update()
+    {
+        motion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rb.velocity = motion * speed;
+    }
+}
